ca51cd47caf5b21e6ebde881e97d0ac459e793a4
Richard David dos Santos <richard-ddsantos@hotmail.com>
2023-03-05 13:04:25 -0300

    commit inicial

---
+++ teste01.js
@@ -0,0 +1,22 @@
+/*
+* 1 - Observe o trecho de código abaixo:
+int INDICE = 13, SOMA = 0, K = 0;
+enquanto K < INDICE faça
+{
+K = K + 1;
+SOMA = SOMA + K;
+}
+imprimir(SOMA);
+Ao final do processamento, qual será o valor da variável SOMA?
+*/
+
+let indice = 13;
+let soma = 0;
+let K = 0;
+
+while (K < indice) {
+  k = k + 1;
+  soma = soma + k;
+}
+
+console.log(soma);
